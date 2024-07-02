661e6364c117d97deda93041c12c5293ebe41e40
Samarth Kamble <kamble.samarth91@gmail.com>
2024-07-02 11:13:56 +0530

    Implement auth with clerk and covex

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,23 @@
+import Image from "next/image";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div>
-        <main>
-          {children}
-        </main>
-      </div>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <main className="relative h-screen w-full ">
+        <div className="absolute size-full">
+          <Image
+            src={"/images/bg-img.png"}
+            className="size-full"
+            fill
+            alt="BG Image"
+          />
+        </div>
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://oriented-mouse-86.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
