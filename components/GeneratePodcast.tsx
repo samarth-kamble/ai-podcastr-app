55376c873dff4e9c68f976b603728b145d545b74
Samarth Kamble <kamble.samarth91@gmail.com>
2024-07-03 00:03:49 +0530

    Implement The create podcast form

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,16 @@
+import { GeneratePodcastProps } from "@/types";
+import React from "react";
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  setAudio,
+  voiceType,
+  audio,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration,
+}: GeneratePodcastProps) => {
+  return <div>GeneratePodcast</div>;
+};
+
+export default GeneratePodcast;
