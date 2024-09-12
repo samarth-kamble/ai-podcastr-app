7c339e8217164638d0f533abf056a17be5e1e945
Samarth Kamble <kamble.samarth91@gmail.com>
2024-09-12 20:35:38 +0530

    Implement Podcast detail Page

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,29 +1,34 @@
+import { PodcastCardProps } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const PodcastCard = ({
-  imgURL,
+  imgUrl,
   title,
   description,
   podcastId,
-}: {
-  imgURL: string;
-  title: string;
-  description: string;
-  podcastId: number;
-}) => {
+}: PodcastCardProps) => {
+  const router = useRouter();
+  const handleViews = () => {
+    // increase view
+    router.push(`/podcast/${podcastId}`, {
+      scroll: true,
+    });
+  };
+
   return (
-    <div className="cursorpoint">
+    <div className="cursor-pointer" onClick={handleViews}>
       <figure className="flex flex-col gap-2">
         <Image
-          src={imgURL}
+          src={imgUrl}
           width={174}
           height={174}
           alt={title}
           className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px]"
         />
         <div className="flex flex-col">
-          <h1 className="text-16 truncate font-bold text-white-1 ">{title}</h1>
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
           <h2 className="text-12 truncate font-normal capitalize text-white-4">
             {description}
           </h2>
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "shiny-pelican-899.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
